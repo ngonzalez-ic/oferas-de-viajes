289fa6a3a133198e63cee46316d2f798935c848e
Ngonzalez-ic <cgonzalez@iiconcepcion.edu.ar>
2019-09-20 17:55:19 -0300

    run time fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Head from './Head';
 import Banner from './Banner';
 import ContainerCards from './ContainerCard';
+import Nuevo from'./requested/nuevo'
 
 
     const App= ()=> {
 
     return <div >
-        <Head/>
-        <Banner/>
-        <ContainerCards/>
+      <Nuevo/>
     </div>
 }
+/* <Head/>
+        <Banner/>
+        <ContainerCards/>*/
 
 export default App;
